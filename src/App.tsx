d109cfa8cf9d9ab51da52761b21083434d84c4fb
Thr33Fingers <thr33fingers@protonmail.com>
2021-02-07 15:46:06 -0800

    Add message

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,24 @@ import Counter from './components/counter';
 import Banger from './components/banger';
 import './App.css';
 
+const getRandomMessage = () => {
+  const messages = [
+    'Civil disobedience movement',
+    'Hear the voice of Myanmar',
+    'Reject military coup',
+    'Respect our votes',
+    'Save Myanmar',
+    'We want democracy',
+    'Release Aung San Su Kyi'
+  ];
+
+  return messages[Math.floor(Math.random() * messages.length)];
+};
+
+
 const App = () => {
   const [counter, setCounter] = useState(Math.floor(new Date().getTime() / 10000000));
+  const [message, setMessage] = useState(getRandomMessage());
   const [irisStyle, setIrisStyle] = useState<React.CSSProperties | undefined>();
   const [bangerStyle, setBangerStyle] = useState<React.CSSProperties | undefined>();
   const bangerRef = useRef<Element>();
@@ -30,11 +46,13 @@ const App = () => {
   const onClick = () => {
     // update counter and start bang animation
     setCounter(counter + 1);
+    setMessage(getRandomMessage());
   };
 
   return (
     <div className='App' onMouseMove={onMouseMove} onClick={onClick}>
       <div className='app-background'/>
+      <h1 className='message'>{message}</h1>
       <div className='min-aung-hlaing'>
         <div className='min-aung-hlaing-inner'>
           <div className='eyes'>
